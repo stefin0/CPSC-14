5c38fe80bc63f3d592eaf26c0df29598d8ad7f5e
stefin <stefinracho@gmail.com>
2023-01-19 14:53:12 -0800

    firstProgram

--- firstProgram.cpp
+++ firstProgram.cpp
@@ -1,8 +1,22 @@
-// Your First C++ Program
+/*
+Stefin Racho
+1/19/23
+First Program - add, subtract, multiply, and divide
+*/
 
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello World 3!";
+    // declare and initialize variables
+    int number1 = 1;
+    int number2 = 2;
+    double number3 = 3;
+
+    cout << "Adding 1 + 2 = " << number1 + number2 << endl;
+    cout << "Subtracting 2 - 3 = " << number2 - number3 << endl;
+    cout << "Multiplying 3 * 1 = " << number3 * number1 << endl;
+    cout << "Dividing 1 / 3 = " << number1 / number3 << endl;
+
     return 0;
 }
